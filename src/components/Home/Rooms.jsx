8e8b9167432a08805e7485787348e9b34248af1c
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-02-19 15:26:45 +0600

    project setup done

---
+++ src/components/Home/Rooms.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+import Container from "../Shared/Container";
+import Heading from "../Shared/Heading";
+import LoadingSpinner from "../Shared/LoadingSpinner";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosCommmon from "../../hooks/useAxiosCommon";
+
+const Rooms = () => {
+  const axiosCommon = useAxiosCommmon();
+
+  const { data: rooms = [], isPending } = useQuery({
+    queryKey: ["rooms"],
+    queryFn: async () => {
+      const { data } = await axiosCommon.get("/rooms");
+      return data;
+    },
+  });
+
+  if (isPending) return <LoadingSpinner />;
+
+  return (
+    <Container>
+      {rooms && rooms.length > 0 ? (
+        <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {rooms.map((room) => (
+            <Card key={room._id} room={room} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center min-h-[calc(100vh-300px)]">
+          <Heading
+            center={true}
+            title="No Rooms Available In This Category!"
+            subtitle="Please Select Other Categories."
+          />
+        </div>
+      )}
+    </Container>
+  );
+};
+
+export default Rooms;
